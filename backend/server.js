df84719b89696a3fa03406aafbe013626c2ec41a
Paul Calderon <paulscalderon@gmail.com>
2024-02-22 00:40:59 -0400

    built signup endpoint

--- backend/server.js
+++ backend/server.js
@@ -3,18 +3,17 @@ import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
+
 const app = express();
-
-dotenv.config();
 const PORT = process.env.PORT || 5000;
+dotenv.config();
 
-app.get("/", (req, res) => {
-    res.send("Hello World!!!!!");
-});
-
+app.use(express.json()); //parse requests from req.body
 app.use("/api/auth", authRoutes);
+
+
 
 app.listen(PORT, () => {
     connectToMongoDB();
-console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
