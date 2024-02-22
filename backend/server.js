2cd656568f3b5702a1989696a04955c4d9548dc2
Paul Calderon <paulscalderon@gmail.com>
2024-02-22 03:22:19 -0400

    created conversational models, message model, message controller , messagfe route and protect route

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
@@ -9,8 +10,10 @@ const PORT = process.env.PORT || 5000;
 dotenv.config();
 
 app.use(express.json()); //parse requests from req.body
-app.use("/api/auth", authRoutes);
+app.use(cookieParser());
 
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 
 app.listen(PORT, () => {
