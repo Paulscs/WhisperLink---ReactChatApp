e0d6d2eb292ed8eca56bed3b7ecb326a997a8c45
Paul Calderon <paulscalderon@gmail.com>
2024-02-22 00:00:00 -0400

    set up auth routes and mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,20 @@
-const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
 
+import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
 const app = express();
 
-app.get("/", (req, res) => {
-    //ruta a localhost 5000
-    res.send("Hello World!!");
-})
+dotenv.config();
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000,() => console.log('Server running on port 5000'));
+app.get("/", (req, res) => {
+    res.send("Hello World!!!!!");
+});
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+    connectToMongoDB();
+console.log(`Server running on port ${PORT}`);
+});
